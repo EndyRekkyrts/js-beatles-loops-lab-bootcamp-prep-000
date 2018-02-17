d71ca4023e4c001ca4bfd8658d8afbdc0c8d9b2d
Carrollyn Catanzaro <endyrekkyrts@yahoo.com>
2018-02-17 21:27:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments) {
-  var arrMusIns{}
+  cons arrMusIns = []
+  
+  for (let i = 0, 1 = arrayMusicians.length; i < 1; i++) {
+    arrMusIns.push('${arrayMusicians[i]} plays ${instruments[i]}')
+  }
+  return arrMusIns
 }
 
 function johnLennonFacts(facts) {
+  const newFacts = []
   
+  let i = 0
+  while (i < facts.length) {
+    newFacts.push('${facts[i]}')
+    i++
+  }
+  return newFacts
 }
 
 function iLoveTheBeatles(number) {
-  var beatles{}
+  const beatles =[]
   
+  do {
+    beatles.push("i love the Beatles!")
+    n++
+  }
+  while (number < 15)
+  
+  return beatles
 }
